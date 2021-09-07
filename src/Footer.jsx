51710b9028c1ab30fa0a843c58d77d08edaf707a
serge_kamya <sergekamya@gmail.com>
2021-09-07 17:29:51 +0200

    installed Semantic UI, added CSS to index.css, modified component files

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const Footer = () => {
+  return (
+    <footer>
+        <p id="footer">Made with React {React.version}</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Menu, Segment } from "semantic-ui-react";
+
+const Header = () => {
+  return (
+    <Segment inverted>
+      <Menu inverted secondary>
+        <Menu.Item id="header" name="my portfolio" />
+      </Menu>
+    </Segment>
+  );
+};
+
+export default Header;
