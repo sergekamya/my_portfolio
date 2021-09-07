df180545aabfefdaf35b90946b0851d326e700e4
serge_kamya <sergekamya@gmail.com>
2021-09-07 22:06:53 +0200

    added spec userCanSeeListOfProjects.feature.js, installed axios, added projects.json file and modified

---
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -0,0 +1,26 @@
+describe("User can see listof projects", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+    cy.get("#projects-tab").click();
+  });
+
+  it("displays the first project", () => {
+    cy.get("#project-1").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".header").should("exist");
+      cy.get("description").should("contain", "My First Website");
+      cy.get(".description").should("contain", "This was my first project.");
+    });
+  });
+
+  it("displays the second project", () => {
+    cy.get("#project-2").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".header").should("contain", "Mobile UX");
+      cy.get(".description").should(
+        "contain",
+        "I like to design for the mobile platform"
+      );
+    });
+  });
+});
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 
-const Projects = () => {
-  return (
-    <Container>
-      <h1 id="projects-header">My Projects</h1>
-      <p>
-        These are my projects! I have built lots of stuff! Hope you like it.
-        These are my projects! I have built lots of stuff! Hope you like it.      </p>
-    </Container>
-  );
-};
+class Projects extends Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        name: "My first Website",
+      },
+      {
+        id: 2,
+        name: "Fizzbuzz",
+      },
+    ],
+  };
+
+  render() {
+    const { projects } = this.state;
+
+    let projectsList = project.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      );
+    });
+
+    return (
+      <Container>
+        <h1 id="projects-header">My Projects</h1>
+        {projectsList}
+      </Container>
+    );
+  }
+}
 
 export default Projects;
