30859f5ea4ec43390987143ac4247941d03effdf
serge_kamya <sergekamya@gmail.com>
2021-09-08 20:30:46 +0200

    removed import Cointaner from Footer.jsx, fixed typos in projects.json, updated userCanSeeListOfProjects spec

--- cypress/integration/userCanSeeListOfProjects.feature.js
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -7,8 +7,7 @@ describe("User can see listof projects", () => {
   it("displays the first project", () => {
     cy.get("#project-1").within(() => {
       cy.get(".image").should("exist");
-      cy.get(".header").should("exist");
-      cy.get("description").should("contain", "My First Website");
+      cy.get(".header").should("contain", "My First Website");
       cy.get(".description").should("contain", "This was my first project.");
     });
   });
@@ -16,10 +15,19 @@ describe("User can see listof projects", () => {
   it("displays the second project", () => {
     cy.get("#project-2").within(() => {
       cy.get(".image").should("exist");
+      cy.get(".header").should("contain", "UI Design");
+      cy.get(".description").should(
+        "contain","Designing user interfaces is fun. I want to learn more about that...");
+    });
+  });
+
+  it("displays the second project", () => {
+    cy.get("#project-3").within(() => {
+      cy.get(".image").should("exist");
       cy.get(".header").should("contain", "Mobile UX");
       cy.get(".description").should(
         "contain",
-        "I like to design for the mobile platform"
+        "I like to design for the mobile platform. The challenges to build UI's for smartphones is challenging but extremly rewarding"
       );
     });
   });
