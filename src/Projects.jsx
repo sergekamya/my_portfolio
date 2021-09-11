59336123a8f37f67bb6073925b893c191a4818a2
serge_kamya <sergekamya@gmail.com>
2021-09-11 11:29:40 +0200

    refactored Projects.jsx from class component to functional component

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,37 +1,72 @@
-import React, { Component } from "react";
+//import React, { Component } from "react";
+import React, {useState, useEffect} from "react";
 import { Container, Grid } from "semantic-ui-react";
 import ProjectCard from "./ProjectCard";
 import axios from "axios";
 
-class Projects extends Component {
-  state = {
-    projects: [],
+
+
+const Projects = () => {
+
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  const fetchProjects = async () => {
+    const response = await axios("./data/projects.json");
+    setProjects(response.data);
   };
 
-  componentDidMount() {
-    axios.get("./data/projects.json").then((response) => {
-      this.setState({ projects: response.data });
-    });
-  }
-
-  render() {
-    const { projects } = this.state;
-
-    let projectsList = projects.map((project) => {
-      return (
-        <div id={`project-${project.id}`} key={project.id}>
-          <ProjectCard project={project} />
-        </div>
-      );
-    });
-
+  let projectsList = projects.map((project) => {
     return (
-      <Container>
-        <h1 id="projects-header">My Projects</h1>
-        <Grid>{projectsList}</Grid>
-      </Container>
+      <div id={`project-${project.id}`} key={project.id}>
+        <ProjectCard project={project} />
+      </div>
     );
-  }
-}
+  });
+
+  return (
+    <Container>
+      <h1 id="projects-header">My Projects</h1>
+      <Grid>{projectsList}</Grid>
+    </Container>
+  );
+};
 
 export default Projects;
+
+
+
+// class Projects extends Component {
+//   state = {
+//     projects: [],
+//   };
+
+//   componentDidMount() {
+//     axios.get("./data/projects.json").then((response) => {
+//       this.setState({ projects: response.data });
+//     });
+//   }
+
+//   render() {
+//     const { projects } = this.state;
+
+//     let projectsList = projects.map((project) => {
+//       return (
+//         <div id={`project-${project.id}`} key={project.id}>
+//           <ProjectCard project={project} />
+//         </div>
+//       );
+//     });
+
+//     return (
+//       <Container>
+//         <h1 id="projects-header">My Projects</h1>
+//         <Grid>{projectsList}</Grid>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default Projects;
